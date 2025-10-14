99c972b26f5575f5cad89fa89fd5843e82082575
shaurya722 <shaurya.joshi@openxcell.com>
2025-10-14 17:12:19 +0530

    footer year changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,13 @@ import { useTranslation } from 'react-i18next'
 
 export default function Footer() {
   const { t } = useTranslation()
-
+  const year = new Date().getFullYear()
   return (
     <footer className='border-t border-[var(--brand-border-weak)] py-12'>
       <div className='container mx-auto px-6'>
         <div className='flex justify-center items-center gap-2'>
           <span className='text-lg font-bold'>
-            {t('footer.copyright')}
+            © {year} {t('footer.copyright')}
           </span>
         </div>
 
